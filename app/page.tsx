2597f0eacf6b51b36cdd8c38d9e80800955de8a1
Test User <test@example.com>
2026-07-07 09:04:01 +0800

    remove display poli12

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,50 @@
-import { Card, Space, Divider, Button, Flex } from 'antd';
-import AppUserWelcomeCard from './(admin)/appUser/page';
-
+import { Card, Space, Divider, Button, Flex } from "antd";
+import AppUserWelcomeCard from "./(admin)/appUser/page";
 
 export default async function Home() {
   return (
-    <Space direction='vertical'>
+    <Space direction="vertical">
       {/* <AppUserWelcomeCard /> */}
       <Space direction="vertical" size="middle">
-        <Card title="Laporan Operasi" size="small"
-        >
-          <Space size="middle" >
+        <Card title="Laporan Operasi" size="small">
+          <Space size="middle">
             <div>Menu untuk cetak Laporan Operasi</div>
-            <Button className="hover:bg-white" type="text" htmlType="submit" href="/laporan/operasi"
-              icon={<i className="ri-arrow-right-circle-line text-3xl"></i>}
-            >
-            </Button>
+            <Button
+              className="hover:bg-white"
+              type="text"
+              htmlType="submit"
+              href="/laporan/operasi"
+              icon={
+                <i className="ri-arrow-right-circle-line text-3xl"></i>
+              }></Button>
           </Space>
         </Card>
       </Space>
-      <Space direction='vertical'>
+      <Space direction="vertical">
         <Card>
           <Space>
             <div>Menu Admin Antrian Poli</div>
-            <Button className="hover:bg-white" type="text" htmlType="submit" href="/antrian/poliAdmin"
-              icon={<i className="ri-arrow-right-circle-line text-3xl"></i>}
-            >
-            </Button>
+            <Button
+              className="hover:bg-white"
+              type="text"
+              htmlType="submit"
+              href="/antrian/poliAdmin"
+              icon={
+                <i className="ri-arrow-right-circle-line text-3xl"></i>
+              }></Button>
           </Space>
         </Card>
         <Card>
           <Space>
             <div>Menu Task Id</div>
-            <Button className="hover:bg-white" type="text" htmlType="submit" href="/autoTaskid"
-              icon={<i className="ri-arrow-right-circle-line text-3xl"></i>}
-            >
-            </Button>
+            <Button
+              className="hover:bg-white"
+              type="text"
+              htmlType="submit"
+              href="/autoTaskid"
+              icon={
+                <i className="ri-arrow-right-circle-line text-3xl"></i>
+              }></Button>
           </Space>
         </Card>
       </Space>
@@ -42,32 +52,44 @@ export default async function Home() {
         <Card>
           <Space>
             <div>Display Antrian All</div>
-            <Button className="hover:bg-white" type="text" htmlType="submit" href="/antrianAll"
-              icon={<i className="ri-arrow-right-circle-line text-3xl"></i>}
-            >
-            </Button>
+            <Button
+              className="hover:bg-white"
+              type="text"
+              htmlType="submit"
+              href="/antrianAll"
+              icon={
+                <i className="ri-arrow-right-circle-line text-3xl"></i>
+              }></Button>
           </Space>
         </Card>
         <Card>
           <Space>
             <div>Display Antrian Admisi</div>
-            <Button className="hover:bg-white" type="text" htmlType="submit" href="/antrianAdmisi"
-              icon={<i className="ri-arrow-right-circle-line text-3xl"></i>}
-            >
-            </Button>
+            <Button
+              className="hover:bg-white"
+              type="text"
+              htmlType="submit"
+              href="/antrianAdmisi"
+              icon={
+                <i className="ri-arrow-right-circle-line text-3xl"></i>
+              }></Button>
           </Space>
         </Card>
         <Card>
           <Space>
             <div>Display Antrian Visus</div>
-            <Button className="hover:bg-white" type="text" htmlType="submit" href="/antrianVisus"
-              icon={<i className="ri-arrow-right-circle-line text-3xl"></i>}
-            >
-            </Button>
+            <Button
+              className="hover:bg-white"
+              type="text"
+              htmlType="submit"
+              href="/antrianVisus"
+              icon={
+                <i className="ri-arrow-right-circle-line text-3xl"></i>
+              }></Button>
           </Space>
         </Card>
 
-        <Card>
+        {/* <Card>
           <Space>
             <div>Display Antrian Poli 1</div>
             <Button className="hover:bg-white" type="text" htmlType="submit" href="/antrianPoli/1"
@@ -84,7 +106,7 @@ export default async function Home() {
             >
             </Button>
           </Space>
-        </Card>
+        </Card> */}
       </Space>
     </Space>
   );
